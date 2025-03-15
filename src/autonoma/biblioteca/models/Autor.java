423afab9142bafd289c4facbf22f1d45989465e1
Cristian Salazar Arenas <cristiancamilosalazararenas@outlook.com>
2025-03-15 11:49:18 -0500

    Se mejoró la clase abstracta llamada Persona,Se creó una nueva clase llamada Autor y se agregó la documentación para ambas clases.

---
+++ src/autonoma/biblioteca/models/Autor.java
@@ -0,0 +1,98 @@
+package autonoma.biblioteca.models;
+
+//Clase Autor
+
+/**
+ * Modelo que permite representar a una clase llamada Autor
+ *
+ * @author Cristian Camilo Salazar Arenas
+ * @version 1.1, 11/03/2025
+ * @since 1.0
+ */
+
+public class Autor extends Persona{
+    
+    /**
+     * Nombre del editorial asociado con el autor
+     */
+    
+    private String editorial;
+    
+    /**
+     * Nombre de la profesión del autor
+     */
+    private String profesion;
+    
+    //Método constructor
+    
+    /**
+     * Este es el método constructor de la clase llamada Autor
+     * 
+     * @param       nombre                  Nombre del autor
+     * @param       documentoIdentidad      Documento de identidad del autor      
+     * @param       correoElectronico       Correo electrónico del autor
+     * @param       editorial               Editorial asociada con el autor
+     * @param       profesion               Profesión del autor
+     * @since       1.0
+     */
+
+    public Autor(String nombre, String documentoIdentidad, String correoElectronico, String editorial, String profesion) {
+        super(nombre, documentoIdentidad, correoElectronico);
+        this.editorial = editorial;
+        this.profesion = profesion;
+    }
+    
+    //Métodos de acceso
+    
+    /**
+     * Accede al nombre de la editorial del autor
+     * 
+     * @return Retorna el nombre de la editorial  
+     * @since 1.0
+     */
+
+    public String getEditorial() {
+        return editorial;
+    }
+    
+    /**
+     * Establece el nombre de la editorial del autor si no está vacío
+     * Si el nombre de la editorial está vacío, no se realiza ningún cambio
+     * 
+     * @param       editorial       Nombre de la editorial
+     * @implNote    Este método no lanza una excepción, pero ignora valores vacíos
+     * @since       1.0
+     */
+
+    public void setEditorial(String editorial) {
+        if (!editorial.isEmpty()){
+            this.editorial = editorial;
+        }
+    }
+    
+    /**
+     * Accede a la profesión del autor
+     * 
+     * @return Retorna la profesión del autor
+     * @since 1.0
+     */
+
+    public String getProfesion() {
+        return profesion;
+    }
+    
+    /**
+     * Establece el nombre de la profesión del autor si no está vacío
+     * Si el nombre de la profesión está vacío, no se realiza ningún cambio
+     * 
+     * @param       profesion       Nombre de la profesión
+     * @implNote    Este método no lanza una excepción, pero ignora valores vacíos
+     * @since 1.0
+     */
+    
+    public void setProfesion(String profesion) {
+        if (!profesion.isEmpty()){
+            this.profesion = profesion;
+        }
+    }   
+}
--- src/autonoma/biblioteca/models/Persona.java
+++ src/autonoma/biblioteca/models/Persona.java
@@ -1,75 +1,131 @@
 package autonoma.biblioteca.models;
 
+//Clase abstracta Persona
 
-///Clase abstracta
 /**
  * Modelo que permite representar a una clase abstracta llamada Persona
  *
  * @author Cristian Camilo Salazar Arenas
- * @version 1.0.0
- * @since 20250311
+ * @version 1.1, 11/03/2025
+ * @since 1.0
  */
 
-abstract class Persona {
+public abstract class Persona {
     
-    ///Atributos
-    /**
-    *Nombre de la persona
-    */
-    private String nombre;
-    /**
-    *Cédula de ciudadania de la persona
-    */
-    private String documentoIdentidad;
-    /**
-    *Correo electrónico de la persona
-    */
-    private String correoElectronico;
-    
-    
-    
-    ///Método constructor
-    /**
-    * Este es el método constructor de la clase abstracta llamada Persona.
-    *
-    * @param     nombre                 Nombre de la persona
-    * @param     documentoIdentidad     Documento de identidad de la persona
-    * @param     correoElectronico      Correo electrónico de la persona
-    * @exception ArithmeticException
-    *            Si el segundo operando es <pre>0</pre>
-    * @see       java.lang.Character#charValue()
-    */
+    //Atributos
 
-    public Persona(String nombre, String documentoIdentidad, String correoElectronico) {
+    /**
+     *Nombre de la persona
+     */
+    
+    protected String nombre;
+    
+    /**
+     *Cédula de ciudadania de la persona
+     */
+    
+    protected String documentoIdentidad;
+    
+    /**
+     *Correo electrónico de la persona
+     */
+    
+    protected String correoElectronico;
+    
+    //Método constructor
+
+    /**
+     * Este es el método constructor de la clase abstracta llamada Persona
+     *
+     * @param     nombre                 Nombre de la persona
+     * @param     documentoIdentidad     Documento de identidad de la persona
+     * @param     correoElectronico      Correo electrónico de la persona
+     * @since 1.0
+     */
+
+    protected Persona(String nombre, String documentoIdentidad, String correoElectronico) {
         this.nombre = nombre;
         this.documentoIdentidad = documentoIdentidad;
         this.correoElectronico = correoElectronico;
     }
-
+    
+    //Métodos de acceso
+    
+    /** 
+     * Accede al nombre de la persona
+     * 
+     * @return retorna el nombre de la persona
+     * @since 1.0
+     */
+    
     public String getNombre() {
         return nombre;
     }
+    
+    /** 
+     * Establece el nombre de la persona si no está vacío
+     * Si el nombre está vacío, no se realiza ningún cambio
+     * 
+     * @param        nombre      Nombre de la persona
+     * @implNote     Este método no lanza una excepción, pero ignora valores vacíos
+     * @since 1.0
+     */
 
     public void setNombre(String nombre) {
-        this.nombre = nombre;
+        if (!nombre.isEmpty()){
+            this.nombre = nombre;
+        } //En el interfaz escribir "Por favor, escriba un nombre"
     }
-
+    
+    /**
+     * Accede al documento de identidad de la persona.
+     * 
+     * @return retorna el documento de identidad de la persona
+     * @since 1.0
+     */
+    
     public String getDocumentoIdentidad() {
         return documentoIdentidad;
     }
+    
+    /**
+     * Establece el documento de identidad de la persona si no está vacío
+     * Si el documento de identidad está vacío, no se realiza ningún cambio
+     * 
+     * @param        documentoIdentidad      Documento de identidad de la persona
+     * @implNote     Este método no lanza una excepción, pero ignora valores vacíos
+     * @since 1.0
+     */
 
     public void setDocumentoIdentidad(String documentoIdentidad) {
-        this.documentoIdentidad = documentoIdentidad;
+        if(!documentoIdentidad.isEmpty()){
+            this.documentoIdentidad = documentoIdentidad;
+        }
     }
+    
+    /**
+     * Accede al correo electrónico de la persona
+     * 
+     * @return retorna el correo electrónico de la persona
+     * @since 1.0
+     */
 
     public String getCorreoElectronico() {
         return correoElectronico;
     }
+    
+    /** 
+     * Establece el correo electrónico de la persona si no está vació
+     * Si el correo electrónico está vacío, no se realiza ningún cambio
+     * 
+     * @param        correoElectronico      Correo electrónico de la persona
+     * @implNote     Este método no lanza una excepción, pero ignora valores vacíos
+     * @since 1.0
+     */
 
     public void setCorreoElectronico(String correoElectronico) {
-        this.correoElectronico = correoElectronico;
-    }
-    
-    //////////////////////
-    ///Métodos de acceso
+        if(!correoElectronico.isEmpty()){
+            this.correoElectronico = correoElectronico;
+        }
+    }   
 }
